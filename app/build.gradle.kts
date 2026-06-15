64c2d31a415f428ea4e29bc369f20f6f9ae8607e
GPLaider <221012486+GPLaider@users.noreply.github.com>
2026-06-16 02:44:26 +0900

    Release Ventoid 0.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.ventoid.app"
         minSdk = 21
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.1.6"
+        versionCode = 8
+        versionName = "0.1.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
